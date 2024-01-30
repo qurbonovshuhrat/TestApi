fb2da88ce912445bd0b2feedb4c4cea32b23e087
qurbonovshuhrat <shuhratqurbonov0507@gmail.com>
2024-01-30 23:00:19 +0500

    salom

--- TestApi/Controllers/HomeController.cs
+++ TestApi/Controllers/HomeController.cs
@@ -31,6 +31,8 @@ namespace TestApi.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
--- TestApi/Controllers/WeatherForecastController.cs
+++ TestApi/Controllers/WeatherForecastController.cs
@@ -26,6 +26,9 @@ namespace TestAPI.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+
+
+            string a = "";
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
